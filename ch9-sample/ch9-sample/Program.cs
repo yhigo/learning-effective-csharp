a925393d1199b7d6cf78d4ea630557e4e626693b
yhigo <4zakura@gmail.com>
2021-05-13 00:34:08 +0900

    first commit

---
+++ ch9-sample/ch9-sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ch9_sample
+{
+    struct Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var attendees = new List<Person>();
+
+            Person p = new Person { Name = "old name" };
+            attendees.Add(p);
+
+            Person p2 = attendees[0];
+            p2.Name = "new name";
+
+            Console.WriteLine(attendees[0].ToString());
+        }
+    }
+}
