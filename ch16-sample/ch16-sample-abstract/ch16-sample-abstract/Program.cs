b702864f10ffb904caa24cee30cb99ccebdb03a4
yhigo <4zakura@gmail.com>
2021-06-07 23:19:26 +0900

    動作確認のためにCh16のabstract版のサンプルコードを作成

---
+++ ch16-sample/ch16-sample-abstract/ch16-sample-abstract/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ch16_sample_abstract
+{
+
+    abstract class B
+    {
+        protected B()
+        {
+            FuncB();
+        }
+
+        abstract protected void FuncB();
+    }
+
+    class Derived : B
+    {
+        private readonly string msg = "初期化子で決定";
+
+        public Derived(string msg)
+        {
+            this.msg = msg;
+        }
+
+        protected override void FuncB()
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var derived = new Derived("called by main");
+        }
+    }
+}
