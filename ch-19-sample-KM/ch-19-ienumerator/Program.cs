db843fda41752dabb21d12f4b35eee6dc0811262
seiyaKM <seiya.kumada.0107@gmail.com>
2021-06-21 11:34:13 +0900

    added chapter 19 sample

---
+++ ch-19-sample-KM/ch-19-ienumerator/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+
+namespace ch_19_ienumerator
+{
+
+    /**
+     * memo
+     * ・IEnumerableは、型が列挙可能(foreach可能)であることを表すためのインターフェイス
+     * ・IEnumeratorはコレクション内の要素を列挙（foreachから取得）する機能を提供するインターフェイス
+     * 　
+     * ・foreach処理中に呼び出される
+     * 　→　反復対象のコレクションを最初に参照したとき・・・IEnumeratorを実装しているクラスの「コンストラクタ」が実行される
+     * 　　　反復対象のコレクションの参照をコピー
+     * 　　　これは最初のだけ
+     * 　→　in が実行されたとき・・・IEnumeratorを実装しているクラスの「MoveNext()」が実行される
+     * 　　　コレクションから値を取り出せるかチェックしている　OKならtrueを返す
+     * 　→　コレクションからオブジェクトを取り出すとき・・・IEnumeratorを実装しているクラスの「Current」プロパティが実行される
+     * 　　　コレクションから取得対象のオブジェクトを取得する　配列からインデックスを指定して取り出す
+     * ・
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+            {
+                new Person("John", "Smith"),
+                new Person("Jim", "Johnson"),
+                new Person("Sue", "Rabon"),
+            };
+
+            People peopleList = new People(peopleArray);
+            foreach (Person p in peopleList)
+            {
+                Console.WriteLine(p.firstName + " " + p.lastName);
+            }
+        }
+    }
+
+    // Simple business object.
+    public class Person
+    {
+        public Person(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+
+        public string firstName;
+        public string lastName;
+    }
+
+    // Collection of Person objects. This class
+    // implements IEnumerable so that it can be used
+    // with ForEach syntax.
+    public class People : IEnumerable
+    {
+        private Person[] _people;
+        public People(Person[] pArray)
+        {
+            _people = new Person[pArray.Length];
+
+            for (int i = 0; i < pArray.Length; i++)
+            {
+                _people[i] = pArray[i];
+            }
+        }
+
+        // Implementation for the GetEnumerator method.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public PeopleEnum GetEnumerator()
+        {
+            return new PeopleEnum(_people);
+        }
+    }
+
+    // When you implement IEnumerable, you must also implement IEnumerator.
+    public class PeopleEnum : IEnumerator
+    {
+        public Person[] _people;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public PeopleEnum(Person[] list)
+        {
+            _people = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _people.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Person Current
+        {
+            get
+            {
+                try
+                {
+                    return _people[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
---
+++ ch-19-sample-KM/ch-19-sample-KM/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ch_19_KM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+            {
+                new Person("John", "Smith"),
+                new Person("Jim", "Johnson"),
+                new Person("Sue", "Rabon"),
+            };
+
+            string[] strArray = new string[3]
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            string moji = "あいうえお";  // ReverseStringEnumeratorで処理される
+
+            var collection = new ReverseEnumerable<char>(moji);
+            foreach (var item in collection)
+            {
+                //Console.WriteLine(item.firstName + " " + item.lastName);
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public Person(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+
+        public string firstName;
+        public string lastName;
+    }
+
+    public sealed class ReverseEnumerable<T> : IEnumerable<T>
+    {
+        private class ReverseEnumerator : IEnumerator<T>
+        {
+
+            int currentIndex;
+            IList<T> collection;
+
+            public ReverseEnumerator(IList<T> srcCollection)
+            {
+                collection = srcCollection;
+                currentIndex = collection.Count;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return collection[currentIndex];
+                }
+            }
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return --currentIndex >= 0;
+            }
+
+            public void Reset()
+            {
+                currentIndex = collection.Count;
+            }
+        }
+
+        // 改善4
+        private sealed class ReverseStringEnumerator : IEnumerator<char>
+        {
+            private string sourceSequence;
+            private int currentIndex;
+
+            public ReverseStringEnumerator(string source)
+            {
+                sourceSequence = source;
+                currentIndex = source.Length;
+            }
+
+            public char Current
+            {
+                get
+                {
+                    return sourceSequence[currentIndex];
+                }
+            }
+
+            object IEnumerator.Current => sourceSequence[currentIndex];
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return --currentIndex >= 0;
+            }
+
+            public void Reset()
+            {
+                currentIndex = sourceSequence.Length;
+            }
+        }
+
+        IEnumerable<T> sourceSequence;
+        IList<T> originalSequence;
+
+        public ReverseEnumerable(IEnumerable<T> sequence)
+        {
+            sourceSequence = sequence;
+
+            // 改善1
+            originalSequence = sequence as IList<T>;
+        }
+
+
+        // 改善2
+        public ReverseEnumerable(IList<T> sequence)
+        {
+            sourceSequence = sequence;
+            originalSequence = sequence;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            // 改善3
+            if (sourceSequence is string)
+            {
+                return new ReverseStringEnumerator(sourceSequence as string) as IEnumerator<T>;
+            }
+
+            if (originalSequence == null)
+            {
+                if (sourceSequence is ICollection<T>)
+                {
+                    ICollection<T> source = sourceSequence as ICollection<T>;
+                }
+                else
+                {
+                    originalSequence = new List<T>();
+                    foreach (T item in sourceSequence)
+                    {
+                        originalSequence.Add(item);
+                    }
+                }
+            }
+
+            //if(originalSequence == null)
+            //{
+            //    originalSequence = new List<T>();
+            //    foreach (T item in sourceSequence)
+            //    {
+            //        originalSequence.Add(item);
+            //    }
+            //}
+
+            return new ReverseEnumerator(originalSequence);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
